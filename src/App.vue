fff16e65c8ccf3189b7a1c6f1a501bcc5aa06435
iMocco <taoxin167@gmail.com>
2019-01-30 19:26:58 +0800

    设置鼠标的样式

--- src/App.vue
+++ src/App.vue
@@ -7,14 +7,9 @@
     <router-view/>
   </div>
 </template>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<script>
+import '@/assets/css/style.css';
+export default {
+  name: 'App'
 }
-</style>
+</script>
