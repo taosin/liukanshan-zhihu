5b3993129cb407b78947c13144a9756aba20ff0b
iMocco <taoxin167@gmail.com>
2019-01-31 18:18:01 +0800

    1. 调整首页导航样式 2. 添加首页背景图

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,14 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home-page">
+  	<main class="main-corridor">
+  	</main>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import '@/assets/css/style.css';
 export default {
   name: 'home',
-  components: {
-    HelloWorld
-  }
 }
 </script>
